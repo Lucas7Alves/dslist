f6104c9c9c89d19a780c119a780a446820cdaa32
Lucas Alves <64313623+Lucas7Alves@users.noreply.github.com>
2025-01-20 21:24:14 -0300

    Game repository; service; controller

---
+++ src/main/java/com/gameproject/dslist/services/GameService.java
@@ -0,0 +1,23 @@
+package com.gameproject.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gameproject.dslist.dto.GameMinDTO;
+import com.gameproject.dslist.entities.Game;
+import com.gameproject.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+	@Autowired
+	private GameRepository gameRepository;
+	
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList(); 
+		return dto;
+	}
+}
